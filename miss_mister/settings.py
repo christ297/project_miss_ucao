2631ed0c077eeedde3be903a6a071dd43bd040f4
christ297 <christbest682@gmail.com>
2025-02-23 23:43:26 +0000

    production

--- miss_mister/settings.py
+++ miss_mister/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%8bw3qf3rbor38d!2t#739+lkhy&6!hed++odfj!7**o&subu9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ['*']
 
@@ -124,11 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_FILES_DIRS = [
-     os.path.join(BASE_DIR, 'static'),
-]
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
